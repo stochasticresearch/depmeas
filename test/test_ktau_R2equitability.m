0b0549732734e35f476f88691554d696ff92484c
Kiran Karra <kiran.karra@gmail.com>
2017-02-28 13:30:39 -0500

    fixed titles to be accurate, moved redundant code.

--- test/test_ktau_R2equitability.m
+++ test/test_ktau_R2equitability.m
@@ -85,6 +85,35 @@ end
 
 title('Equitability Curves for \tau', 'FontSize', fontSize);
 grid on;
-xlabel('\Phi', 'FontSize', fontSize); ylabel('RSDM', 'FontSize', fontSize);
+xlabel('\Phi', 'FontSize', fontSize); ylabel('\tau', 'FontSize', fontSize);
 h = legend('Y = X','Y = e^X', 'Location', 'NorthWest');
 set(h,'FontSize', fontSize);
+
+%% Plot Figure 5 - which shows the effect of statistical ranks and noise
+
+clear;
+clc;
+
+rng(123);
+
+fontSize = 20;
+
+M = 500;
+x = rand(M,1)*10+2;
+y1 = x; y2 = exp(x);
+sigma = 2;
+noise = sigma*randn(M,1);
+
+u = pobs(x);
+v1 = pobs(y1); v1_n = pobs(y1+noise);
+v2 = pobs(y2); v2_n = pobs(y2+noise);
+
+subplot(2,2,1); scatter(x,y1); grid on;
+xlabel({'x','(a)'}, 'FontSize', 20); ylabel('y', 'FontSize', 20);
+subplot(2,2,2); scatter(u,v1_n); grid on;
+xlabel({'u','(b)'}, 'FontSize', 20); ylabel('v', 'FontSize', 20);
+
+subplot(2,2,3); scatter(x,y2); grid on;
+xlabel({'x','(c)'}, 'FontSize', 20); ylabel('y', 'FontSize', 20);
+subplot(2,2,4); scatter(u,v2_n); grid on;
+xlabel({'u','(d)'}, 'FontSize', 20); ylabel('v', 'FontSize', 20);
