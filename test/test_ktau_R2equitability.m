31021fbe2d5650ac40f60fd07367c20087e34308
Kiran Karra <kiran.karra@gmail.com>
2017-02-28 13:09:26 -0500

    added generation of more figures for the paper, and r2 equitability test for ktau

---
+++ test/test_ktau_R2equitability.m
@@ -0,0 +1,90 @@
+%**************************************************************************
+%*                                                                        *
+%* Copyright (C) 2016  Kiran Karra <kiran.karra@gmail.com>                *
+%*                                                                        *
+%* This program is free software: you can redistribute it and/or modify   *
+%* it under the terms of the GNU General Public License as published by   *
+%* the Free Software Foundation, either version 3 of the License, or      *
+%* (at your option) any later version.                                    *
+%*                                                                        *
+%* This program is distributed in the hope that it will be useful,        *
+%* but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+%* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+%* GNU General Public License for more details.                           *
+%*                                                                        *
+%* You should have received a copy of the GNU General Public License      *
+%* along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+%*                                                                        *
+%**************************************************************************
+
+clear;
+clc;
+
+rng(123);
+
+% Generate the equitability plot for Kendall's Tau
+xi = 0.01:0.01:0.99;
+
+nrow = 99; ncol = 100;
+
+res_f1 = zeros(nrow, ncol);
+res_f2 = zeros(nrow, ncol);
+
+R = 0.01;
+Rinv = 1/R;
+M = 500;
+
+dispstat('','init'); % One time only initialization
+dispstat(sprintf('Begining the equitability simulation...\n'),'keepthis','timestamp');
+
+for ii=1:nrow
+    dispstat(sprintf('%0.02f %% Complete',ii/nrow*100),'timestamp');
+    parfor jj=1:ncol       
+        x = rand(M,1)*10+2;
+        y = x;
+        noise = randn(M,1)*sqrt(var(y)*(Rinv-1));
+        y = y+noise;
+        res_f1(ii,jj) = rsdm(x,y);
+        
+        x = rand(M,1)*10+2;
+        y = exp(x);
+        noise = randn(M,1)*sqrt(var(y)*(Rinv-1));
+        y = y+noise;
+        res_f2(ii,jj) = rsdm(x,y);
+    end
+    
+    R = R + 0.01;
+    Rinv = 1/R;
+end
+
+% save the data
+if(ispc)
+    save('C:\\Users\\Kiran\\ownCloud\\PhD\\sim_results\\independence\\equitability_ktau.mat');
+elseif(ismac)
+    save('/Users/Kiran/ownCloud/PhD/sim_results/independence/equitability_ktau.mat');
+else
+    save('/home/kiran/ownCloud/PhD/sim_results/independence/equitability_ktau');
+end
+
+%% load the data and plot
+if(ispc)
+    load('C:\\Users\\Kiran\\ownCloud\\PhD\\sim_results\\independence\\equitability_ktau.mat');
+elseif(ismac)
+    load('/Users/Kiran/ownCloud/PhD/sim_results/independence/equitability_ktau.mat');
+else
+    load('/home/kiran/ownCloud/PhD/sim_results/independence/equitability_ktau');
+end
+
+fontSize = 20;
+
+hold on;
+for jj=1:99
+    plot(xi, res_f1(:,jj), 'g*');
+    plot(xi, res_f2(:,jj), 'b*');
+end
+
+title('Equitability Curves for \tau', 'FontSize', fontSize);
+grid on;
+xlabel('\Phi', 'FontSize', fontSize); ylabel('RSDM', 'FontSize', fontSize);
+h = legend('Y = X','Y = e^X', 'Location', 'NorthWest');
+set(h,'FontSize', fontSize);
