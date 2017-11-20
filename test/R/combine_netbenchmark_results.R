e37e73a7910e4d2990bb9ef232897d6c0722d539
Kiran <kiran.karra@gmail.com>
2017-11-19 23:37:13 -0500

    updated netbenchmark tests to consider results of sweeps

--- test/R/combine_netbenchmark_results.R
+++ test/R/combine_netbenchmark_results.R
@@ -34,41 +34,54 @@ arrange.vars <- function(data, vars){
 }
 
 if(Sys.info()["sysname"]=="Darwin") {  # mac
-  rResultsRepo_cnr = "/Users/kiran/data/netbenchmark/r_outputs"
-  combinedResultsRepo_cnr = "/Users/kiran/data/netbenchmark/combined_outputs"
+  rResultsRepo_cnr = "/data/netbenchmark/r_outputs"
+  combinedResultsRepo_cnr = "/data/netbenchmark/combined_outputs"
 } else {  # unix
-  rResultsRepo_cnr = "/home/kiran/data/netbenchmark/r_outputs"
-  combinedResultsRepo_cnr = "/home/kiran/data/netbenchmark/combined_outputs"
+  rResultsRepo_cnr = "/data/netbenchmark/r_outputs"
+  combinedResultsRepo_cnr = "/data/netbenchmark/combined_outputs"
 }
 
-numDatasetsToProcess = 150
+numDatasetsToProcess = 200
+globalNoiseVec = c(0,10,20,30,40,50)
+localNoiseVec = c(10,20,30,40,50)
 
-# for(ds in c("syntren300","rogers1000","syntren1000","gnw1565","gnw2000")) {
-for(ds in c("syntren300")) {
+#availableDataSources = c("syntren300","rogers1000","syntren1000","gnw1565","gnw2000")
+availableDataSources = c("syntren300")
+
+for(ds in availableDataSources) {
   # create an empty dataframe for this datasource, we merge each
   # individual result into this overall dataframe, and store the
   # merged output
-  merged_df = data.frame()
-  for(i in seq(numDatasetsToProcess)) {
-    # load the R estimators data
-    rf = file.path(rResultsRepo_cnr,sprintf("%s_%d.Rdata",ds,i))
-    load(rf)
-    # load the Matlab estimator data
-    mf = file.path(rResultsRepo_cnr,sprintf("%s_%d_matlab.Rdata",ds,i))
-    load(mf)
-    # merge
-    top20.aupr$`AUPRtop20%`$CIM   = cim_top20.aupr$`AUPRtop20%`$CIM
-    top20.aupr$`AUPRtop20%`$KNN1  = knn1_top20.aupr$`AUPRtop20%`$MatlabMI
-    top20.aupr$`AUPRtop20%`$KNN6  = knn6_top20.aupr$`AUPRtop20%`$MatlabMI
-    top20.aupr$`AUPRtop20%`$KNN20 = knn20_top20.aupr$`AUPRtop20%`$MatlabMI
-    top20.aupr$`AUPRtop20%`$vME   = vme_top20.aupr$`AUPRtop20%`$MatlabMI
-    top20.aupr$`AUPRtop20%`$AP    = ap_top20.aupr$`AUPRtop20%`$MatlabMI
-    
-    merged_df <- rbind(merged_df, top20.aupr$`AUPRtop20%`)
+  for(global_noise in globalNoiseVec) {
+    for(local_noise in localNoiseVec) {
+      subfolder = sprintf("gn_%d_ln_%d",global_noise,local_noise)
+      # check to see if the matlab processing is available
+      if(file.exists(file.path(rResultsRepo_cnr,subfolder,sprintf('%s_%d_matlab.Rdata',ds,numDatasetsToProcess)))) {
+        print(sprintf('Processing ds=%s [%d/%d]',ds,global_noise,local_noise))
+        merged_df = data.frame()
+        for(i in seq(numDatasetsToProcess)) {
+          # load the R estimators data
+          rf = file.path(rResultsRepo_cnr,subfolder,sprintf("%s_%d.Rdata",ds,i))
+          load(rf)
+          # load the Matlab estimator data
+          mf = file.path(rResultsRepo_cnr,subfolder,sprintf("%s_%d_matlab.Rdata",ds,i))
+          load(mf)
+          # merge
+          top20.aupr$`AUPRtop20%`$CIM   = cim_top20.aupr$`AUPRtop20%`$CIM
+          top20.aupr$`AUPRtop20%`$KNN1  = knn1_top20.aupr$`AUPRtop20%`$MatlabMI
+          top20.aupr$`AUPRtop20%`$KNN6  = knn6_top20.aupr$`AUPRtop20%`$MatlabMI
+          top20.aupr$`AUPRtop20%`$KNN20 = knn20_top20.aupr$`AUPRtop20%`$MatlabMI
+          top20.aupr$`AUPRtop20%`$vME   = vme_top20.aupr$`AUPRtop20%`$MatlabMI
+          top20.aupr$`AUPRtop20%`$AP    = ap_top20.aupr$`AUPRtop20%`$MatlabMI
+          
+          merged_df <- rbind(merged_df, top20.aupr$`AUPRtop20%`)
+        }
+        # store off merged data
+        merged_df <- arrange.vars(merged_df, c("CIM"=1,"Kendall"=2,"KNN1"=3,"KNN6"=4,"KNN20"=5,"vME"=6,"AP"=7))
+        print(merged_df)
+        fOut = file.path(combinedResultsRepo_cnr,sprintf("%s_combined_gn_%d_ln_%d.csv",ds,global_noise,local_noise))
+        write.table(merged_df,fOut,sep=",",row.names=FALSE)
+      }
+    }
   }
-  # store off merged data
-  merged_df <- arrange.vars(merged_df, c("CIM"=1,"Kendall"=2,"KNN1"=3,"KNN6"=4,"KNN20"=5,"vME"=6,"AP"=7))
-  print(merged_df)
-  fOut = file.path(combinedResultsRepo_cnr,sprintf("%s_combined.csv",ds))
-  write.table(merged_df,fOut,sep=",",row.names=FALSE)
 }
